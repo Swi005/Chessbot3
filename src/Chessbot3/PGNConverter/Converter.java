d08450cf642edb9f712222feb6ae2511cc67035f
Sander Wiig <sander.wiig@student.uib.no>
2020-10-07 15:45:29 +0200

    Added WiP Converter

---
+++ src/Chessbot3/PGNConverter/Converter.java
@@ -0,0 +1,142 @@
+import Chessbot3.GameBoard.Board;
+import Chessbot3.MiscResources.Move;
+import Chessbot3.MiscResources.Tuple;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+    public class Converter {
+        static Board board = new Board();
+
+        public Converter() {
+        }
+
+        static Move convertToMove(String str) {
+            String chars = "abcdefgh";
+            String badstuff = "x+#*";
+            boolean extraInfo = false;
+            Tuple<Integer, Integer> partialInfo = null;
+            char[] temp = str.toCharArray();
+            str = "";
+            char[] var6 = temp;
+            int var7 = temp.length;
+
+            for(int var8 = 0; var8 < var7; ++var8) {
+                char c = var6[var8];
+                if (badstuff.indexOf(c) == -1) {
+                    str = str + Character.toString(c);
+                }
+            }
+
+            System.out.println(str);
+            char fst = str.charAt(0);
+            String endofmove = "";
+            if (str.equals("O-O")) {
+                return null;
+            } else if (str.equals("O-O-O")) {
+                return null;
+            } else {
+                if (str == "Be7") {
+                    System.out.println("test");
+                }
+
+                if (Character.isUpperCase(fst)) {
+                    endofmove = str.substring(1);
+                } else {
+                    if (Character.isDigit(fst)) {
+                        return null;
+                    }
+
+                    endofmove = str;
+                    fst = 'P';
+                }
+
+                Tuple endPos;
+                if (endofmove.length() > 2) {
+                    extraInfo = true;
+                    if (endofmove.length() == 4) {
+                        endPos = new Tuple(chars.indexOf(endofmove.charAt(0)), 8 - chars.indexOf(endofmove.charAt(1)));
+                        Tuple<Integer, Integer> end = new Tuple(chars.indexOf(endofmove.charAt(2)), 8 - chars.indexOf(endofmove.charAt(3)));
+                        return new Move(endPos, end);
+                    }
+
+                    if (endofmove.length() == 3) {
+                        if (Character.isDigit(endofmove.charAt(0))) {
+                            partialInfo = new Tuple(-1, 8 - chars.indexOf(endofmove.charAt(0)));
+                        } else {
+                            partialInfo = new Tuple(chars.indexOf(endofmove.charAt(0)), -1);
+                        }
+
+                        endofmove = endofmove.substring(1);
+                    }
+                }
+
+                endPos = new Tuple(chars.indexOf(endofmove.charAt(0)), 8 - Integer.parseInt(endofmove.substring(1)));
+                List<Move> posMoves = board.genCompletelyLegalMoves(board.getColorToMove());
+                posMoves = pruneMoves(posMoves, endPos);
+                Iterator var10 = posMoves.iterator();
+
+                Move m;
+                char sym;
+                do {
+                    if (!var10.hasNext()) {
+                        return null;
+                    }
+
+                    m = (Move)var10.next();
+                    sym = Character.toUpperCase(board.getPiece((Integer)m.getX().getX(), (Integer)m.getX().getY()).getSymbol());
+                } while(sym != fst);
+
+                Move move;
+                if (extraInfo && (Integer)partialInfo.getX() == -1) {
+                    if (m.getX().getY() == partialInfo.getY()) {
+                        move = new Move(m.getX(), endPos);
+                        board.movePiece(move);
+                        return move;
+                    }
+
+                    if ((Integer)partialInfo.getY() == -1 && m.getX().getX() == partialInfo.getX()) {
+                        move = new Move(m.getX(), endPos);
+                        board.movePiece(move);
+                        return move;
+                    }
+                }
+
+                move = new Move(m.getX(), endPos);
+                board.movePiece(move);
+                return move;
+            }
+        }
+
+        static List<Move> pruneMoves(List<Move> list, Tuple<Integer, Integer> endPos) {
+            List<Move> retList = new ArrayList();
+            Iterator var3 = list.iterator();
+
+            while(var3.hasNext()) {
+                Move m = (Move)var3.next();
+                if (m.getY().equals(endPos)) {
+                    retList.add(m);
+                }
+            }
+
+            return retList;
+        }
+
+        public static void main(String[] args) {
+            String[] var1 = Parser.parseMoves(new File("C:\\Users\\Sande\\Documents\\INF101\\Chessbot3\\src\\Chessbot3\\files\\test.pgn"));
+            int var2 = var1.length;
+
+            for(int var3 = 0; var3 < var2; ++var3) {
+                String s = var1[var3];
+                Move move = convertToMove(s);
+                if (move != null) {
+                    System.out.println(move.toAlgebraicNotation());
+                } else {
+                    System.out.println("Casteling");
+                }
+            }
+
+        }
+    }
+
---
+++ src/Chessbot3/PGNConverter/Parser.java
@@ -0,0 +1,49 @@
+package Chessbot3.PGNConverter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Parser {
+    public Parser() {
+    }
+
+    static String[] parseMoves(File file) {
+        List<String> moves = new ArrayList();
+        String allMoves = "";
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.length() > 0 && line.charAt(0) != '[') {
+                    allMoves = allMoves + line;
+                }
+            }
+        } catch (IOException var8) {
+            System.out.println(var8.getMessage());
+            var8.printStackTrace();
+        }
+
+        allMoves = allMoves.replaceAll("\\{([\\s\\S]*?)\\}", "");
+        String[] subStrings = allMoves.split("([0-9]+[.]) |([\\s])");
+        String[] var10 = subStrings;
+        int var5 = subStrings.length;
+
+        for(int var6 = 0; var6 < var5; ++var6) {
+            String str = var10[var6];
+            if (!str.equals("")) {
+                moves.add(str);
+            }
+        }
+
+        return (String[])moves.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        parseMoves(new File("C:\\Users\\Sande\\Documents\\INF101\\Chessbot3\\src\\Chessbot3\\files\\test.pgn"));
+    }
+}
+
